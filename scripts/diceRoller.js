5ac90fc737ef0b9e05f0465b488c39b43eab4b5b
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2025-01-05 17:26:23 +0000

    ADD: Dice roller

---
+++ scripts/diceRoller.js
@@ -0,0 +1,35 @@
+function getDieRolls(numberOfDice, numberOfSides) {
+    const rolls = [];
+    for (let i = 0; i < numberOfDice; i++) {
+        const roll = Math.floor(Math.random() * numberOfSides) + 1;
+        rolls.push(roll);
+    }
+    return rolls;
+}
+
+function isNumeric(input) {
+    return /^\d+$/.test(input);
+}
+
+function submit() {
+    const numberOfDice = document.getElementById('number-of-dice').value;
+    const numberOfSides = document.getElementById('minimum-length').value;
+
+    if (!isNumeric(numberOfDice) || Math.floor(numberOfDice) === numberOfDice || numberOfDice <= 0) {
+        return alert('Please enter a valid whole number for number of dice.');
+    }
+
+    if (!isNumeric(numberOfSides) || Math.floor(numberOfSides) === numberOfSides || numberOfSides <= 0) {
+        return alert('Please enter a valid whole number for number of sides.');
+    }
+
+    const dieRolls = getDieRolls(numberOfDice, numberOfSides);
+
+    alert(dieRolls);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('roll-dice').addEventListener('click', () => {
+        submit();
+    });
+});
